6033524ddd31957fb989e1b5510819c53d7802e0
AVIRAL108 <vrlgarg@gmail.com>
2021-11-27 14:27:40 +0530

    fourth commit

---
+++ p5.java
@@ -0,0 +1,9 @@
+public class p5
+{public static void main()
+    {int s=1;
+    {for(int i=1;i<=5;i++)
+        {for(int j=1;j<=s;j++)
+            System.out.print("*");
+            System.out.println();
+            s++;
+        }}}}
